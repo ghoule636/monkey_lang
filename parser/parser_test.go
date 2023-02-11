00f7c362ff6b6fe75b06ac2b27228ab60b900d13
Gabe <ghoule636@gmail.com>
2023-02-10 20:56:23 -0800

    added some logic tothe parser.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,18 +8,19 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-	let x = 5;
-	let y = 10;
-	let foobar = 838383
-	`
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
 	l := lexer.New(input)
 	p := New(l)
+
 	program := p.ParseProgram()
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
 	if len(program.Statements) != 3 {
-		t.Fatalf("program.Statements does not contain 3 states. gpt %d",
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
 			len(program.Statements))
 	}
 
@@ -41,22 +42,23 @@ func TestLetStatements(t *testing.T) {
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
-		t.Errorf("s.TokenLiteral not 'let' got %q", s.TokenLiteral())
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
 		return false
 	}
 
 	letStmt, ok := s.(*ast.LetStatement)
 	if !ok {
-		t.Errorf("s not *ast.LetStatment. got %T", s)
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
 		return false
 	}
+
 	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not %s. got %s", name, letStmt.Name.Value)
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
 		return false
 	}
 
 	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("letStmt.Name.TokenLiteral() not %s. got %s",
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
 			name, letStmt.Name.TokenLiteral())
 		return false
 	}
