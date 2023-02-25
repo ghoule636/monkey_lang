5b249a3d43651ebb5b6531b7ca78d1970f95e7cc
Gabe <ghoule636@gmail.com>
2023-02-24 18:36:04 -0800

    adding some error handling. Discovered I have a seg fault error for some reason when the parser is failing.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,18 @@ import (
 	"testing"
 )
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser errors :%q", msg)
+	}
+	t.FailNow()
+}
+
 func TestLetStatements(t *testing.T) {
 	input := `
 let x = 5;
